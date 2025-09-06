1c2e8d7d552e613139f79f5125fbeb13360d7cba
JulioPMartin <julio.martin@aluno.ifsp.edu.br>
2025-09-05 21:29:32 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,9 +15,17 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html', current_time=datetime.utcnow())
+@app.route('/', methods=['GET', 'POST')
+def index():
+    name = None;
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name "= form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<nome>')
 def user(nome):
